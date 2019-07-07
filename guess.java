38b84e73a518d46c36ecb647056bf524030f6e0f
hancun <52615349+hancun@users.noreply.github.com>
2019-07-07 10:41:42 +0800

    Add files via upload

---
+++ guess.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class guess{
+	public static void main(String[] args){
+
+		int computer=(int)(Math.random()*3)+1;
+		
+			System.out.println(computer);
+
+   }
+}
